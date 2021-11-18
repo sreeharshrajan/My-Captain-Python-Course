51ff800c2ad5727b5d36cbc2a87e2982ea7d6222
sreeharshrajan <sreeharshkrajan@gmail.com>
2021-11-18 16:11:09 +0530

    assignment 1

---
+++ assignment_1.py
@@ -0,0 +1,13 @@
+# find x and y from below sequence 10,1,8,3,6,5,4,7,x,y
+x = 10
+y = 1
+while x >= 1:
+    print(x, ',',  y,  end="")
+    x = x-2
+    y = y+2
+
+# Algorithm for Finding X and Y
+# 1. Set x=10 and y=1
+# 2. Loop if X is greater than or eqqual to 1
+# 2.1. Print X and Y while decrementing 2 from X and incrementing 2 from y
+# 3. Loop will stop when x becomes 0
---
+++ frequency.py
@@ -0,0 +1,14 @@
+W = input('Please enter a string ')
+
+
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+
+print("Frequency List ", most_frequent(W))
